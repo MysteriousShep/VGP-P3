7e51d6b5db2a07b913465c2ae2a61ed4954a147d
MysteriousShep <95491119+MysteriousShep@users.noreply.github.com>
2022-10-13 10:20:49 -0700

    Prototype 2 up to challenge 2

--- Prototype 2/Assets/Scripts/MoveForwards.cs
+++ Prototype 2/Assets/Scripts/MoveForwards.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class MoveForwards : MonoBehaviour
 {
+    public float maxSpeed = 40.0f;
+    public float minSpeed = 40.0f;
     public float speed = 40.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(minSpeed,maxSpeed);
     }
 
     // Update is called once per frame
