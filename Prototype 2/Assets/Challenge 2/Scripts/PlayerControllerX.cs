1ae85b6dd9f814337f158204c9f4458de52277ca
MysteriousShep <95491119+MysteriousShep@users.noreply.github.com>
2022-10-28 10:35:48 -0700

    Challenge 2 minor change

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-    public GameObject[] balls;
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            int ballIndex = Random.Range(0,balls.Length)
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
