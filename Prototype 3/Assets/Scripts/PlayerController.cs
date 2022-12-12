a6185c7aa82a362f603332e0e6e1e9546654ef9d
MysteriousShep <95491119+MysteriousShep@users.noreply.github.com>
2022-12-12 10:21:59 -0800

    Prototype 3 Finish

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 100;
     public float gravityModifier = 1;
     public bool isOnGround = true;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,7 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(Vector3.up*jumpForce);
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,9 +31,11 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && isOnGround && !gameOver)
         {
+            playerAudio.PlayOneShot(jumpSound,1.0f);
             isOnGround = false;
             rb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
 
@@ -35,6 +44,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else
         if (collision.gameObject.CompareTag("Obstacle"))
@@ -43,6 +53,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b",true);
             playerAnim.SetInteger("DeathType_int",1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound,1.0f);
         }
     }
 }
