4d7c4a8b1fab123045cee0f537562fbf10665b6d
MysteriousShep <95491119+MysteriousShep@users.noreply.github.com>
2023-01-25 14:03:44 -0800

    Custom engine horizontal collisions

--- Personal Project 2D/Assets/Scripts/PlayerController.cs
+++ Personal Project 2D/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,11 @@ public class PlayerController : MonoBehaviour
     public int jumpDuration = 20;
     public float gravity;
     public int coyoteTime = 0;
+    public int accelleration = 3;
+    public int deccelleration = 3;
+    private float xVelocity = 0;
+    private float xSpeed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,29 +37,23 @@ public class PlayerController : MonoBehaviour
         Vector3 min = new Vector2(transform.position.x-0.5f,transform.position.y-0.8f);
         Vector2 corner1 = new Vector2(max.x-0.05f,min.y-0.0f+yVelocity);
         Vector2 corner2 = new Vector2(min.x+0.05f,min.y+1.0f+yVelocity);
-        if (grounded)
-        {
-            corner1 = new Vector2(max.x-0.05f,min.y-0.05f);
-            corner2 = new Vector2(min.x+0.05f,min.y+1.0f);
-        }
         Collider2D hit = Physics2D.OverlapArea(corner1,corner2);
 
-        Debug.DrawLine(new Vector3(corner1.x,corner1.y,0),new Vector3(corner2.x,corner1.y,0),Color.red);
-        Debug.DrawLine(new Vector3(corner2.x,corner1.y,0),new Vector3(corner2.x,corner2.y,0),Color.red);
-        Debug.DrawLine(new Vector3(corner2.x,corner2.y,0),new Vector3(corner1.x,corner2.y,0),Color.red);
-        Debug.DrawLine(new Vector3(corner1.x,corner2.y,0),new Vector3(corner1.x,corner1.y,0),Color.red);
+        
 
         grounded = false;
-        if (hit != null && hit.gameObject != gameObject) {
-            grounded = true;
-            transform.Translate(new Vector3(0,yVelocity,0));
+        if (hit != null && hit.gameObject != gameObject) 
+        {
+            
+            
             corner1 = new Vector2(max.x-0.05f,min.y-0.0f);
             corner2 = new Vector2(min.x+0.05f,min.y+1.0f);
             hit = Physics2D.OverlapArea(corner1,corner2);
             for (int i = 0; i < 30; i++)
             {
-                if (hit != null && hit.gameObject != gameObject) {
-                    transform.Translate(new Vector3(0,0.01f,0));
+                if (hit == null) 
+                {
+                    transform.Translate(new Vector3(0,0.01f*Mathf.Sign(yVelocity),0));
                     max = new Vector2(transform.position.x+0.5f,transform.position.y+0.5f);
                     min = new Vector2(transform.position.x-0.5f,transform.position.y-0.8f);
                     corner1 = new Vector2(max.x-0.05f,min.y-0.0f);
@@ -62,59 +61,40 @@ public class PlayerController : MonoBehaviour
                     hit = Physics2D.OverlapArea(corner1,corner2);
                 }
             }
+            yVelocity = 0;
+            max = new Vector2(transform.position.x+0.5f,transform.position.y+0.5f);
+            min = new Vector2(transform.position.x-0.5f,transform.position.y-0.9f);
+            corner1 = new Vector2(max.x-0.05f,min.y-0.0f);
+            corner2 = new Vector2(min.x+0.05f,min.y+1.0f);
+            hit = Physics2D.OverlapArea(corner1,corner2);
+            if (hit != null && hit.gameObject != gameObject)
+            {
+                grounded = true;
+            }
         }
+        Debug.DrawLine(new Vector3(corner1.x,corner1.y,0),new Vector3(corner2.x,corner1.y,0),Color.red);
+        Debug.DrawLine(new Vector3(corner2.x,corner1.y,0),new Vector3(corner2.x,corner2.y,0),Color.red);
+        Debug.DrawLine(new Vector3(corner2.x,corner2.y,0),new Vector3(corner1.x,corner2.y,0),Color.red);
+        Debug.DrawLine(new Vector3(corner1.x,corner2.y,0),new Vector3(corner1.x,corner1.y,0),Color.red);
         yVelocity -= gravity*Time.deltaTime;
-        if (grounded) {
+        if (grounded) 
+        {
             jumpFrame = 0;
             coyoteFrame = 0;
             yVelocity = 0;
-            if (movement.x != 0)
+            if (xSpeed != 0)
             {
                 playerAnimator.SetTrigger("Run");
-                if (movement.x < 0)
-                {
-                    GetComponent<SpriteRenderer>().flipX = true;
-                }
-                else
-                {
-                    GetComponent<SpriteRenderer>().flipX = false;
-                }
+                
             }
             else
             {
                 playerAnimator.SetTrigger("Idle");
             }
         }
-        if (jumpFrame < jumpDuration && movement.y > 0 && coyoteFrame < coyoteTime) {
-            jumpFrame += 1;
-            yVelocity = jumpSpeed;
-            playerAnimator.SetTrigger("Jump");
-            max = new Vector2(transform.position.x+0.5f,transform.position.y+0.5f);
-            min = new Vector2(transform.position.x-0.5f,transform.position.y-0.4f);
-            corner1 = new Vector2(max.x-0.05f,min.y-0.0f+yVelocity);
-            corner2 = new Vector2(min.x+0.05f,min.y+1.0f+yVelocity);
-            hit = Physics2D.OverlapArea(corner1,corner2);
-
-            Debug.DrawLine(new Vector3(corner1.x,corner1.y,0),new Vector3(corner2.x,corner1.y,0),Color.blue);
-            Debug.DrawLine(new Vector3(corner2.x,corner1.y,0),new Vector3(corner2.x,corner2.y,0),Color.blue);
-            Debug.DrawLine(new Vector3(corner2.x,corner2.y,0),new Vector3(corner1.x,corner2.y,0),Color.blue);
-            Debug.DrawLine(new Vector3(corner1.x,corner2.y,0),new Vector3(corner1.x,corner1.y,0),Color.blue);
-
-            
-            if (hit != null && hit.gameObject != gameObject) {
-                yVelocity = 0;
-                Debug.Log(hit.ToString());
-            }
-        } else if (jumpFrame < jumpDuration && (jumpFrame > 0 || coyoteFrame >= coyoteTime) && !grounded) {
-            jumpFrame += 2;
-        }
-        else if (!grounded && coyoteFrame < coyoteTime)
+        else
         {
-            coyoteFrame += 1;
-        }
-        if (!grounded)
-        {
-            if (yVelocity < -0.0f)
+            if (yVelocity < -0.05f)
             {
                 playerAnimator.SetTrigger("Fall");
             }
@@ -127,6 +107,104 @@ public class PlayerController : MonoBehaviour
                 playerAnimator.SetTrigger("Hang");
             }
         }
-        transform.Translate(new Vector3(movement.x * speed,yVelocity,0));
+        if (jumpFrame < jumpDuration && movement.y > 0 && coyoteFrame < coyoteTime) 
+        {
+            jumpFrame += 1;
+            yVelocity = jumpSpeed;
+            playerAnimator.SetTrigger("Jump");
+            grounded = false;
+        } 
+        else if (jumpFrame < jumpDuration && (jumpFrame > 0 || coyoteFrame >= coyoteTime) && !grounded) 
+        {
+            jumpFrame += 2;
+        }
+        else if (!grounded && coyoteFrame < coyoteTime)
+        {
+            coyoteFrame += 1;
+        }
+        transform.Translate(new Vector3(0,yVelocity,0));
+        if (movement.x != 0)
+        {
+            if (movement.x < 0)
+            {
+                if (xSpeed > 0)
+                {
+                    xSpeed -= speed/accelleration;
+                }
+                if (xSpeed > -speed)
+                {
+                    xSpeed -= speed/accelleration;
+                }
+            }
+            else
+            {
+                if (xSpeed < 0)
+                {
+                    xSpeed += speed/accelleration;
+                }
+                if (xSpeed < speed)
+                {
+                    xSpeed += speed/accelleration;
+                }
+            }
+        }
+        else
+        {
+            if (xSpeed < 0)
+            {
+                xSpeed += speed/deccelleration;
+                if (xSpeed > 0)
+                {
+                    xSpeed = 0;
+                }
+            }
+            if (xSpeed > 0)
+            {
+                xSpeed -= speed/deccelleration;
+                if (xSpeed < 0)
+                {
+                    xSpeed = 0;
+                }
+            }
+        }
+        if (xSpeed < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (xSpeed > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        playerAnimator.SetFloat("xSpeed",Mathf.Abs(xSpeed/speed));
+        xVelocity = xSpeed;
+        max = new Vector2(transform.position.x+0.5f,transform.position.y+0.5f);
+        min = new Vector2(transform.position.x-0.5f,transform.position.y-0.7f);
+        corner1 = new Vector2(max.x-0.05f+xSpeed,min.y-0.0f);
+        corner2 = new Vector2(min.x+0.05f+xSpeed,min.y+1.0f);
+        hit = Physics2D.OverlapArea(corner1,corner2);
+        if (hit != null && hit.gameObject != gameObject) 
+        {
+            
+            
+            corner1 = new Vector2(max.x-0.05f,min.y-0.0f);
+            corner2 = new Vector2(min.x+0.05f,min.y+1.0f);
+            hit = Physics2D.OverlapArea(corner1,corner2);
+            for (int i = 0; i < 30; i++)
+            {
+                if (hit == null) 
+                {
+                    transform.Translate(new Vector3(0.01f*Mathf.Sign(xVelocity),0,0));
+                    max = new Vector2(transform.position.x+0.5f,transform.position.y+0.5f);
+                    min = new Vector2(transform.position.x-0.5f,transform.position.y-0.7f);
+                    corner1 = new Vector2(max.x-0.05f,min.y-0.0f);
+                    corner2 = new Vector2(min.x+0.05f,min.y+1.0f);
+                    hit = Physics2D.OverlapArea(corner1,corner2);
+                }
+            }
+            transform.Translate(new Vector3(-0.01f*Mathf.Sign(xVelocity),0,0));
+            xVelocity = 0;
+            xSpeed = 0;
+        }
+        transform.Translate(new Vector3(xVelocity,0,0));
     }
 }
